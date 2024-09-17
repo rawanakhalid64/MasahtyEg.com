9e97725517b4575ae66f9b0669cc5aae845a1e2a
unknown <rawankhalid557@gmail.com>
2024-09-17 13:36:50 +0300

    whoUs component updates & dropdown mobile styling

--- components/mobileDropDownContact/mobileDropDown.tsx
+++ components/mobileDropDownContact/mobileDropDown.tsx
@@ -1,6 +1,6 @@
-// components/MobileDropdown.tsx
-"use client"; // Declare this as a client component
+"use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 interface MobileDropdownProps {
@@ -8,27 +8,38 @@ interface MobileDropdownProps {
   tabs: { id: string; label: string }[];
 }
 
-export default function MobileDropdown({ activeTab, tabs }: MobileDropdownProps) {
+export default function CustomDropdown({ activeTab, tabs }: MobileDropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(activeTab);
   const router = useRouter();
 
-  const handleTabChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedTab = event.target.value;
-    router.push(`?tab=${selectedTab}`);
+  const handleSelectTab = (tabId: string) => {
+    setSelectedTab(tabId);
+    setIsOpen(false);
+    router.push(`?tab=${tabId}`);
   };
 
   return (
-    <div className="w-full mt-4 mb-6">
-      <select
-        className="w-full p-3 border border-gray-300 rounded-lg"
-        value={activeTab}
-        onChange={handleTabChange}
+    <div className="relative w-full">
+      <button
+        className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#198754] focus:border-[#198754]"
+        onClick={() => setIsOpen(!isOpen)}
       >
-        {tabs.map((tab) => (
-          <option key={tab.id} value={tab.id}>
-            {tab.label}
-          </option>
-        ))}
-      </select>
+        {tabs.find(tab => tab.id === selectedTab)?.label || "Select an option"}
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 w-full mt-2  !list-none bg-white border border-gray-300 rounded-lg shadow-lg">
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className="p-3 hover:bg-[#198754] hover:text-white cursor-pointer"
+              onClick={() => handleSelectTab(tab.id)}
+            >
+              {tab.label}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
